f61f4e238c5d0411ba9c5dcecdf3c95d2e3dc874
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-09-29 23:09:16 +0000

    Attempted to do quadMinus and quadPlus

--- functionPractice.py
+++ functionPractice.py
@@ -7,14 +7,15 @@
 # Formula H
 def pythag(a, b):
 	return (a ** 2 + b ** 2) ** .5
-print(pythag(1, 2))
 
 # Formula I
 def discriminant(a, b, c):
 	return (b ** 2) - (4 * a * c)
-print(discriminant(1, 2, 3))
 
 # Formula J
 def quadPlus(a, b, c):
-	return 
-print(quadPlus(1, 2, 3))
+	return (-b + (b ** 2 - (4 * a * c) ** 0.5))/(2 * a)
+
+# Formula K
+def quadMinus(a, b, c):
+	return (-b - (b ** 2 - (4 * a * c) ** 0.5))/(2 * a)
